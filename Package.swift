c17e4aaa16382cb726333e829654da09a4863e87
Bahadır Enes Atay <benesatay@gmail.com>
2023-08-13 01:39:37 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "PresentationModal",
+    platforms: [
+        .iOS(.v13)
+       ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PresentationModal",
             targets: ["PresentationModal"]),
     ],
+    dependencies: [
+           // Dependencies declare other packages that this package depends on.
+            .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.6.0"),
+       ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PresentationModal"),
+            name: "PresentationModal",
+            dependencies: ["SnapKit"],
+            path: "Sources/"
+        ),
         .testTarget(
             name: "PresentationModalTests",
             dependencies: ["PresentationModal"]),
---
+++ Sources/PresentationModal/HeaderView.swift
@@ -0,0 +1,110 @@
+//
+//  HeaderView.swift
+//  
+//
+//  Created by Bahadır Enes Atay on 12.08.2023.
+//
+
+import UIKit
+import SnapKit
+
+protocol HeaderViewDelegate: AnyObject {
+    func didBackButtonTapped()
+    func setSeperatorStyle(_ view: UIView)
+    func setCloseButtonStyle(_ button: UIButton)
+}
+
+extension HeaderViewDelegate {
+    func setSeperatorStyle(_ view: UIView) {}
+    func setCloseButtonStyle(_ button: UIButton) {}
+}
+
+//protocol HeaderViewDataSource: AnyObject {
+//    func setBackground()
+//}
+
+class HeaderView: UIView {
+    
+    // MARK: - Views
+    public lazy var closeButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(didBackButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var seperatorView: UIView = {
+        let seperatorView = UIView()
+        seperatorView.backgroundColor = .systemGray4
+        seperatorView.layer.cornerRadius = 2
+        return seperatorView
+    }()
+    
+    // MARK: - Public Properties
+    weak var delegate: HeaderViewDelegate?
+    
+//    weak var dataSource: HeaderViewDataSource?
+    
+    // MARK: - Init
+    init() {
+        super.init(frame: .zero)
+        setViewAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private Methods
+    private func setViewAppearance() {
+        backgroundColor = .white
+        layer.cornerRadius = 20
+        layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    // MARK: - Public Methods
+    public func setCloseButton() {
+        setCloseButtonConstraints()
+        setCloseButtonStyle()
+    }
+    
+    public func setSeperator() {
+        setSeperatorContstraints()
+        setSeperatorStyle()
+    }
+    
+    private func setCloseButtonConstraints() {
+        seperatorView.removeFromSuperview()
+        seperatorView.snp.removeConstraints()
+        addSubview(closeButton)
+        closeButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(16)
+            make.top.equalToSuperview().inset(safeAreaInsets.top + 16)
+            make.leading.equalToSuperview().inset(16)
+        }
+    }
+    
+    private func setSeperatorContstraints() {
+        closeButton.removeFromSuperview()
+        closeButton.snp.removeConstraints()
+        addSubview(seperatorView)
+        seperatorView.snp.makeConstraints({ make in
+            make.height.equalTo(4)
+            make.width.equalTo(44)
+            make.center.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(24)
+        })
+    }
+    
+    private func setSeperatorStyle() {
+        delegate?.setSeperatorStyle(seperatorView)
+    }
+    
+    private func setCloseButtonStyle() {
+        delegate?.setCloseButtonStyle(closeButton)
+    }
+    
+    // MARK: - Actions
+    @objc private func didBackButtonTapped() {
+        delegate?.didBackButtonTapped()
+    }
+}
